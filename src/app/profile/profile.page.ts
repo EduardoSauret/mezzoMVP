33bf57ba0003b585486111a479bcc5731265c8fe
Diego Magallon <diegomagallon0325@gmail.com>
2019-08-13 20:05:18 -0500

    Adding 'logout' and 'Delete account' buttons

--- src/app/profile/profile.page.ts
+++ src/app/profile/profile.page.ts
@@ -2,6 +2,10 @@ import { Component, OnInit } from '@angular/core';
 import { AngularFirestore, AngularFirestoreDocument } from '@angular/fire/firestore'
 import { UserService } from '../user.service';
 import { Router } from '@angular/router';
+import { AlertController} from '@ionic/angular';
+import { AngularFireAuth } from '@angular/fire/auth'
+import { resolve } from 'url';
+import { reject } from 'q';
 
 @Component({
 	selector: 'app-page',
@@ -17,14 +21,41 @@ export class ProfilePage implements OnInit {
 	username: string
 	profilePic: string
 
-	constructor(private afs: AngularFirestore, private user: UserService, private router: Router) {
+	constructor(public afAuth: AngularFireAuth, private afs: AngularFirestore, private user: UserService, private router: Router, public alertController: AlertController ) {
 		this.mainuser = afs.doc(`users/${user.getUID()}`)
 		this.sub = this.mainuser.valueChanges().subscribe(event => {
 			this.posts = event.posts
 			this.username = event.username
 			this.profilePic = event.profilePic
 		})
+		
 	}
+
+	//Action popup for the logout
+	async logOutAlert(){
+		const alert = await this.alertController.create({
+			header: 'Leaving so soon?',
+			buttons: ['Yes', "No"]
+		});
+
+		await alert.present();
+	}
+
+	
+	
+	//Log out Firebase 
+	async logout() {
+
+
+		console.log("First");
+		this.afAuth.auth.signOut();
+		console.log("Success")
+
+	}
+
+
+	
+
 
 	ngOnDestroy() {
 		this.sub.unsubscribe()
