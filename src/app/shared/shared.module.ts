b18ebd1b8daae6a7e32b86f560273443852f1f8c
eduardo sauret <eduardo.sauret@gmail.com>
2019-11-22 01:36:47 -0500

    sound profile header

--- src/app/shared/shared.module.ts
+++ src/app/shared/shared.module.ts
@@ -1,7 +1,10 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { IonicModule } from '@ionic/angular';
+import { FlexLayoutModule } from '@angular/flex-layout';
+import { SuperTabsModule } from '@ionic-super-tabs/angular';
 
+// Custom Ionic components
 import { AudioListComponent } from './audio-list/audio-list.component';
 import { PhotoListComponent } from './photo-list/photo-list.component';
 import { VideoListComponent } from './video-list/video-list.component';
@@ -14,12 +17,16 @@ import { VideoListComponent } from './video-list/video-list.component';
 	],
 	imports: [
 		CommonModule,
-		IonicModule
+		IonicModule,
+		FlexLayoutModule,
+		SuperTabsModule
 	],
 	exports: [
 		AudioListComponent,
 		PhotoListComponent,
-		VideoListComponent
+		VideoListComponent,
+		FlexLayoutModule,
+		SuperTabsModule
 	]
 })
 export class SharedModule { }
--- src/app/sound/sound-profile/sound-profile.module.ts
+++ src/app/sound/sound-profile/sound-profile.module.ts
@@ -3,10 +3,10 @@ import { CommonModule } from '@angular/common';
 import { FormsModule } from '@angular/forms';
 import { Routes, RouterModule } from '@angular/router';
 import { IonicModule } from '@ionic/angular';
-import { SuperTabsModule } from '@ionic-super-tabs/angular';
 
 import { SoundProfilePage } from './sound-profile.page';
 import { SharedModule } from '../../shared/shared.module';
+
 
 const routes: Routes = [
 	{
@@ -18,7 +18,6 @@ const routes: Routes = [
 @NgModule({
 	imports: [
 		CommonModule,
-		SuperTabsModule,
 		FormsModule,
 		IonicModule,
 		RouterModule.forChild(routes),
