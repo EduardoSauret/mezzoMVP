617c47f27b0f341a7511e251b3f25e22fa18f261
eduardo sauret <eduardo.sauret@gmail.com>
2022-12-03 11:08:00 -0500

    photo-list-ui

--- src/app/sound/sound-profile/sound-profile.page.ts
+++ src/app/sound/sound-profile/sound-profile.page.ts
@@ -1,21 +1,41 @@
 import { AfterViewInit, Component, Input, ViewChild } from '@angular/core';
 
 @Component({
-	selector: 'app-sound-profile',
-	templateUrl: './sound-profile.page.html',
-	styleUrls: ['./sound-profile.page.scss'],
+  selector: 'app-sound-profile',
+  templateUrl: './sound-profile.page.html',
+  styleUrls: ['./sound-profile.page.scss'],
 })
 export class SoundProfilePage implements AfterViewInit {
 
+  photos: any[];
 
-	constructor(
-		// private popoverCtrl: PopoverController
-		) {
-	}
+  constructor(
+    // private popoverCtrl: PopoverController
+  ) {
+    this.photos = [];
+    this.photos.push({
+      src: '../../assets/images/profile/profile-01.jpg',
+    });
+    this.photos.push({
+      src: '../../assets/images/profile/profile-02.jpg',
+    });
+    this.photos.push({
+      src: '../../assets/images/profile/profile-03.jpg',
+    });
+    this.photos.push({
+      src: '../../assets/images/profile/profile-04.jpg',
+    });
+    this.photos.push({
+      src: '../../assets/images/profile/profile-05.jpg',
+    });
+    this.photos.push({
+      src: '../../assets/images/profile/profile-06.jpg',
+    });
+  }
 
-	ngAfterViewInit() {
-		// console.log('Super tabs is ', this.superTabs);
-		// this.superTabs.selectTab(1);
-	}
+  ngAfterViewInit() {
+    // console.log('Super tabs is ', this.superTabs);
+    // this.superTabs.selectTab(1);
+  }
 
 }
