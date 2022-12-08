d37ef2bcf361aa9ac206182b953655b52a625aac
eduardo sauret <eduardo.sauret@gmail.com>
2022-12-08 13:22:25 -0500

    sound-upload-page

--- src/app/sound/sound-upload/sound-upload.module.ts
+++ src/app/sound/sound-upload/sound-upload.module.ts
@@ -3,9 +3,11 @@ import { CommonModule } from '@angular/common';
 import { FormsModule } from '@angular/forms';
 import { Routes, RouterModule } from '@angular/router';
 
+
 import { IonicModule } from '@ionic/angular';
 
 import { SoundUploadPage } from './sound-upload.page';
+import { SharedModule } from 'src/app/shared/shared.module';
 
 const routes: Routes = [
 	{
@@ -19,6 +21,7 @@ const routes: Routes = [
 		CommonModule,
 		FormsModule,
 		IonicModule,
+		SharedModule,
 		RouterModule.forChild(routes)
 	],
 	declarations: [SoundUploadPage]
