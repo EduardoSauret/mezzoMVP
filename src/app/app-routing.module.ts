cbbecfb342a336883889a9cf8ac6944b1cf53360
eduardo sauret <eduardo.sauret@gmail.com>
2023-09-21 15:01:17 -0400

    register new user

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -9,11 +9,12 @@ import { AuthService } from './auth.service';
 // ];
 
 const routes: Routes = [
-	{ path: '', redirectTo: 'sound', pathMatch: 'full' },
+	{ path: '', redirectTo: 'register', pathMatch: 'full' },
 	{ path: 'login', loadChildren: './login/login.module#LoginPageModule' },
 	{ path: 'register', loadChildren: './register/register.module#RegisterPageModule' },
 	{ path: 'tabs', loadChildren: './tabs/tabs.module#TabsPageModule', canActivate: [AuthService] },
 	{ path: 'new-profile', loadChildren: './new-profile/new-profile.module#NewProfilePageModule', canActivate: [AuthService] },
+	{ path: 'edit-profile', loadChildren: './edit-profile/edit-profile.module#EditProfilePageModule' },
 	{ path: 'sound', loadChildren: './sound/sound-tabs/sound-tabs.module#SoundTabsPageModule'},
 	{ path: 'influencers', loadChildren: './actors/actors-tabs/actors-tabs.module#ActorsTabsPageModule' },
 	{ path: 'content-creators', loadChildren: './content-creators/content-creators-tabs/content-creators-tabs.module#ContentCreatorsTabsPageModule' },
--- src/app/user.service.ts
+++ src/app/user.service.ts
@@ -2,23 +2,40 @@ import { Injectable } from '@angular/core';
 import { AngularFireAuth } from '@angular/fire/auth';
 import { first } from 'rxjs/operators';
 import { auth } from 'firebase/app';
+import { Observable } from 'rxjs';
+import { AngularFirestore, DocumentReference } from '@angular/fire/firestore';
 
-interface User {
+export interface User {
 	username: string;
 	uid: string;
+}
+
+export interface UserProfile {
+	uid: string;  // Firebase UID
+  username: string;
+  bio: string;
+  artist: string[];  // Array of artist data
+  creative: string[];  // Array of creative data
+  influencer: string[];  // Array of influencer data
 }
 
 @Injectable()
 export class UserService {
 	private user: User;
+	profileType: string[];
+	user$: Observable<firebase.User>;
 
-	constructor(private afAuth: AngularFireAuth) {
-
+	constructor(private afAuth: AngularFireAuth, private firestore: AngularFirestore) {
+		this.user$ = this.afAuth.authState;
 	}
 
 	setUser(user: User) {
 		this.user = user;
 	}
+
+	getCurrentUser(): Observable<firebase.User | null> {
+    return this.afAuth.authState;
+  }
 
 	getUsername(): string {
 		return this.user.username;
@@ -55,4 +72,22 @@ export class UserService {
 	getUID(): string {
 		return this.user.uid;
 	}
+
+	setProfileType(profileType: string[]){
+		this.profileType = profileType;
+	}
+
+	getProfileType(){
+		return this.profileType;
+	}
+
+	addUserProfile(userProfile: UserProfile): Promise<void> {
+		const { uid, ...userData } = userProfile;  // Extract UID and other data
+		return this.firestore.collection('userProfiles').doc(uid).set(userData);
+	}
+
+  getUserProfile(uid: string): Observable<any> {
+    return this.firestore.collection('userProfiles').doc(uid).valueChanges();
+  }
+
 }
