693d57331f91b7f7750bd1b1c2cf5b2d0a77962e
eduardo sauret <eduardo.sauret@gmail.com>
2023-09-23 02:16:04 -0400

    login-navigate

--- src/app/login/login.page.ts
+++ src/app/login/login.page.ts
@@ -1,20 +1,23 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnDestroy, OnInit } from '@angular/core';
 import { AngularFireAuth } from '@angular/fire/auth';
 import { auth } from 'firebase/app';
 import { UserProfile, UserService } from '../user.service';
 import { Router } from '@angular/router';
+import { Subscription } from 'rxjs';
 
 @Component({
 	selector: 'app-login',
 	templateUrl: './login.page.html',
 	styleUrls: ['./login.page.scss'],
 })
-export class LoginPage implements OnInit {
+export class LoginPage implements OnInit, OnDestroy {
 
 	username = '';
 	email = '';
 	password = '';
 	verifyPassword = '';
+	userProfileSubscription: Subscription;
+	profileType = '';
 
 	constructor(public afAuth: AngularFireAuth, public userService: UserService, public router: Router) { }
 
@@ -23,23 +26,39 @@ export class LoginPage implements OnInit {
 	}
 
 	onSubmit(){
-		//TODO: get userprofile from firebase and get profileType
-		// const profileType = this.user.getProfileType();
-		const profileType = 'artists';
-		// this.router.navigate([`${profileType}/profile`]);
-
 		this.afAuth.auth.signInWithEmailAndPassword(this.email, this.password)
 		.then((userCredential) => {
 			const userCredentials = userCredential.user;
 			this.userService.setUserProfileDataFromFirestore(userCredentials.uid);
-			
-
-      this.router.navigate([`${profileType}/profile`]);
+			this.userService.getUserProfile(userCredentials.uid);
+			this.getUserProfile(userCredentials.uid);
     })
     .catch(error => {
 			console.log('Login Error');
       console.log(error);
     });
+	}
+
+
+	getUserProfile(uid): void {
+    const userId = uid;
+    this.userProfileSubscription = this.userService.getUserProfile(userId).subscribe(
+      userProfileData => {
+        // Process the userProfileData
+				this.profileType = userProfileData.profileType;
+				this.router.navigate([`/${this.profileType}s/profile`]);
+      },
+      error => {
+        // Handle the error
+        console.error(error);
+      }
+    );
+  }
+
+	ngOnDestroy() {
+		if(this.userProfileSubscription){
+			this.userProfileSubscription.unsubscribe();
+		}
 	}
 
 }
